39d396828c1a097106cb721bc2193a78da48d9f0
Xujin Ba <baxujin2023@sjtu.edu.cn>
2024-03-23 20:20:30 +0800

    code save

--- demo_save.cpp
+++ demo_save.cpp
@@ -1,3 +1,17 @@
+// 构造函数
+// 压入从给定点出发的所有可能边，和构成的path
+
+// next()
+// 循环
+// 栈顶取出，目前path保存成一个结果
+
+// 根据取出的对，压所有可能的边，加入需要保证上界，和构成新的path
+
+// 刚才保存的结果范围正确，返回，否则继续循环
+
+// hasnext()
+// stack非空
+
 #include <algorithm>
 #include <fstream>
 #include <iostream>
@@ -13,35 +27,35 @@ struct Edge {
 };
 
 enum class Direction { IN, OUT };
-enum class IterState { START, CONSUMING, FINISH };
+// enum class IterState { START, CONSUMING, FINISH };
 
 class Graph {
 private:
-    int V;                          // 顶点数
-    vector<vector<int>> adjMatrix;  // 邻接矩阵表示图
-    vector<int> nextNeighbor;  // 存储每个顶点的下一个邻接节点
+    int V;
+    vector<vector<int>> adjMatrix;
 
 public:
     Graph(int V) : V(V) {
-        adjMatrix.resize(
-            V, vector<int>(V, 0));  // 初始化邻接矩阵，所有元素设为0表示无连接
-        nextNeighbor.resize(V, 0);  // 初始化 nextNeighbor 向量
+        adjMatrix.resize(V, vector<int>(V, 0));
     }
 
+    // get g[u][v]
+    // TODO check in range
     int getW(int u, int v) {
         return adjMatrix[u][v];
     }
 
+    // get number of node
     int getV() {
         return V;
     }
 
-    // 添加边
-    void addEdge(int u, int v) {
-        adjMatrix[u][v] = 1;
+    // add edge
+    void addEdge(int u, int v, int weight) {
+        adjMatrix[u][v] = weight;
     }
 
-    // 打印图的邻接矩阵
+    // print Graph
     void printGraph() {
         cout << "Adjacency Matrix:" << endl;
         for (int i = 0; i < V; ++i) {
@@ -51,153 +65,63 @@ public:
             cout << endl;
         }
     }
-
-    // 获取下一个邻接节点
-    int getNext(int current, const vector<int>& currentPath) {
-        for (int i = nextNeighbor[current]; i < V; ++i) {
-            if (adjMatrix[current][i] &&
-                find(currentPath.begin(), currentPath.end(), i) ==
-                    currentPath.end()) {
-                nextNeighbor[current] = i + 1;  // 更新下一个邻接节点的位置
-                return i;
-            }
-        }
-        return -1;  // 如果没有未访问的邻接节点，返回-1
-    }
-
-    // 非递归 深度优先搜索
-    void DFS(int start, pair<int, int> pathLengthRange) {
-        stack<pair<int, vector<int>>> stack;
-        stack.push({start, {start}});
-
-        while (!stack.empty()) {
-            int current = stack.top().first;
-            vector<int> currentPath = stack.top().second;
-            stack.pop();
-
-            int currentPathLength = currentPath.size() - 1;
-            if (currentPathLength >= pathLengthRange.first &&
-                currentPathLength <= pathLengthRange.second) {
-                // 输出路径
-                for (int i = 0; i < currentPath.size() - 1; ++i) {
-                    cout << currentPath[i] << " -> ";
-                }
-                cout << currentPath.back() << endl;
-            }
-
-            if (currentPathLength >= pathLengthRange.second)
-                continue;
-
-            // 获取下一个未访问的邻接节点
-            int next = getNext(current, currentPath);
-            while (next != -1) {
-                vector<int> newPath = currentPath;
-                newPath.push_back(next);
-                stack.push({next, newPath});
-                next = getNext(current, currentPath);
-            }
-        }
-    }
 };
 
 class PathIterator {
 private:
-    int start;
     pair<int, int> pathLengthRange;
-    IterState currentstate;
-    // stack<pair<int, vector<pair<Edge, Direction>>>> stack;
-    // pair<int, vector<pair<Edge, Direction>>> current_stack_unit;
-    // vector<pair<Edge, Direction>> currentPath;
-    // pair<Edge, Direction> currentPair;
 
-    int currentnode = -1;
-    vector<pair<Edge, Direction>> stack;
+    stack<vector<pair<Edge, Direction>>> stack;
     vector<pair<Edge, Direction>> currentPath;
-    pair<Edge, Direction> currentPair;
 
-    vector<int> nextNeighbor;
-
-    bool has_out = false;
+    int currentNode = -1;
 
 public:
-    Graph graph;
+    Graph& graph;
     PathIterator(Graph& g, int start, pair<int, int> pathLengthRange)
-        : graph(g), start(start), pathLengthRange(pathLengthRange) {
-        currentnode = start;
-        currentstate = IterState::START;
-        nextNeighbor.resize(g.getV(), 0);
+        : graph(g), pathLengthRange(pathLengthRange) {
+        currentNode = start;
+        for (int i = 0; i < g.getV(); i++) {
+            if (graph.getW(currentNode, i) != 0) {
+                pair<Edge, Direction> newPair;
+                newPair.first = {currentNode, i, graph.getW(currentNode, i)};
+                newPair.second = Direction::OUT;
+                stack.push({newPair});
+            }
+        }
     }
 
-    bool next(vector<pair<Edge, Direction>>& output) {
-        while (!stack.empty() || currentstate == IterState::START) {
-            if (currentstate == IterState::START) {
-                for (int i = nextNeighbor[currentnode]; i < graph.getV(); i++) {
-                    if (graph.getW(currentnode, i) != 0) {
-                        nextNeighbor[currentnode] = i + 1;
-                        pair<Edge, Direction> newPair;
-                        newPair.first = {currentnode, i,
-                                         graph.getW(currentnode, i)};
-                        newPair.second = Direction::OUT;
-                        stack.push_back(newPair);
-                        break;
-                    }
-                }
-                currentstate = IterState::CONSUMING;
-                continue;
-            }
+    void next(vector<pair<Edge, Direction>>& output) {
+        while (hasNext()) {
+            currentPath = stack.top();
+            stack.pop();
+            currentNode = currentPath.back().first.t;
             
-            if (has_out) {
-                for (int i = nextNeighbor[currentnode]; i < graph.getV(); i++) {
-                    if (graph.getW(currentnode, i) != 0) {
-                        nextNeighbor[currentnode] = i + 1;
+            int currentPathLen = currentPath.size();
+
+            if (currentPathLen < pathLengthRange.second) {
+                for (int i = 0; i < graph.getV(); i++) {
+                    if (graph.getW(currentNode, i) != 0) {
+                        auto newPath = currentPath;
                         pair<Edge, Direction> newPair;
-                        newPair.first = {currentnode, i,
-                                         graph.getW(currentnode, i)};
+                        newPair.first = {currentNode, i,
+                                         graph.getW(currentNode, i)};
                         newPair.second = Direction::OUT;
-
-                        stack.push_back(newPair);
-
-                        has_out = false;
-
-                        break;
+                        newPath.push_back(newPair);
+                        stack.push(newPath);
                     }
                 }
             }
-
-            currentPair = stack.back();
-            currentnode = currentPair.first.t;
-
-            currentPath = stack;
-            int currentPathLength = currentPath.size();
-            if (has_out == false &&
-                currentPathLength >= pathLengthRange.first &&
-                currentPathLength <= pathLengthRange.second) {
-                // set output and return true
+            if (currentPathLen >= pathLengthRange.first &&
+                currentPathLen <= pathLengthRange.second) {
                 output = currentPath;
-                has_out = true;
-                return true;
+                return;
             }
-            if (currentPathLength > pathLengthRange.second) {
-                stack.pop_back();
-                stack.pop_back();
-                has_out = true;
-                continue;
-            }
-
-            // nextNeighbor[currentnode] = 0;
-            // for (int i = nextNeighbor[currentnode]; i < graph.getV(); i++) {
-            //     if (graph.getW(currentnode, i) != 0) {
-            //         nextNeighbor[currentnode] = i + 1;
-            //         pair<Edge, Direction> newPair;
-            //         newPair.first = {currentnode, i,
-            //                          graph.getW(currentnode, i)};
-            //         newPair.second = Direction::OUT;
-
-            //         stack.push_back(newPair);
-            //     }
-            // }
         }
-        return false;
+    }
+
+    bool hasNext() {
+        return !stack.empty();
     }
 };
 
@@ -213,18 +137,14 @@ int main() {
         return 1;
     }
 
-    int V, E;  // 顶点数、边数
+    int V, E;
     inputFile >> V >> E;
-
     Graph g(V);
-
-    // 读取边
     for (int i = 0; i < E; ++i) {
-        int u, v;
-        inputFile >> u >> v;
-        g.addEdge(u, v);
+        int u, v, weight;
+        inputFile >> u >> v >> weight;
+        g.addEdge(u, v, weight);
     }
-
     g.printGraph();
 
     int startVertex;
@@ -238,19 +158,21 @@ int main() {
 
     auto iter = getIterator(g, startVertex, pathLengthRange);
     vector<pair<Edge, Direction>> output;
-    while (iter.next(output)) {
-        for (int i = 0; i < output.size(); ++i) {
+    while (iter.hasNext()) {
+        iter.next(output);
+        for (int i = 0; i < output.size(); i++) {
             cout << output[i].first.s;
             if (output[i].second == Direction::OUT) {
-                cout << " -> ";
-            } else {
-                cout << " <- ";
-            }
-        }
-        cout << output.back().first.t << endl;
-    }
+                cout << "-[" << output[i].first.w << "]->";
 
-    // g.DFS(startVertex, pathLengthRange);
+            } else {
+                cout << "<-[" << output[i].first.w << "]-";
+            }
+            cout << output[i].first.t << ' ';
+        }
+        cout << endl;
+        // cout << output.back().first.t << endl;
+    }
 
     return 0;
 }
